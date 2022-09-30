7040544230d0059df4ee040f8ef20c48dd8e7f10
Teresa Soria <terence_line@hotmail.fr>
2022-09-30 18:12:00 +0200

    ajout css et styles banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,14 @@
+import '../styles/Banner.css'
+
 function Banner() {
-    return <h1>La maison jungle</h1>
+    return <div className='lmj-banner'>
+        <h1>La maison jungle</h1>
+    </div>
 }
 
 export default Banner
+
+
+
+
+

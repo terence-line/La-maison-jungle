bc961b82a70ec0e7fa2639d049d92b8e42dfd1c8
Teresa Soria <terence_line@hotmail.fr>
2022-10-01 13:07:24 +0200

    ajout fichier care plantlist

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
 import Banner from './Banner'
-import Cart from './Cart'
+import logo from '../assets/monstera.jpg'
+// import Cart from './Cart'
 import ShoppingList from './ShoppingList'
+
 
 function App() {
   return (
     <div>
-      <Banner />
-      <Cart />
+      <Banner>
+        <img src={logo} alt='logo-la-maison-jungle' />
+        <h1 className='lmj-title'>La maison jungle</h1>
+      </Banner>
+      {/* <Cart /> */}
       <ShoppingList />
     </div>
   )
 }
-
 export default App
-
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,19 +1,31 @@
-const plantList = [
-    'monstera',
-    'ficus lyrata',
-    'pothos argenté',
-    'yucca',
-    'palmier'
-]
+import { plantList } from '../datas/plantList'
+import Care from './Care'
+import '../styles/ShoppingList.css'
 
 function ShoppingList() {
+    const categories = plantList.reduce(
+        (acc, elem) =>
+            acc.includes(elem.category) ? acc : acc.concat(elem.category),
+        []
+    )
     return (
-        <ul>
-            {plantList.map((plant, index) => (
-                <li key={`${plant}-${index}`}>{plant}</li>
-            ))}
-        </ul>
+        <div>
+            <ul>
+                {categories.map((cat) => (
+                    <li key={cat}>{cat}</li>
+                ))}
+            </ul>
+            <ul className='lmj-plant-list'>
+                {plantList.map((plant) => (
+                    <li key={plant.id} className='lmj-plant-item'>
+                        {plant.bestSale && <span>🔥</span>}
+                        {plant.name}
+                        <Care careType='water' care={plant.water} />
+                        <Care careType='light' care={plant.light} />
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
-
 export default ShoppingList

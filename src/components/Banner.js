f30dede11b53011540c8512dc10dbda6d4f82d4d
Teresa Soria <terence_line@hotmail.fr>
2022-10-01 23:24:18 +0200

    ajouts fichiers

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,17 +1,9 @@
-import monstera from '../assets/monstera.jpg'
 import '../styles/Banner.css'
 
 
-function Banner() {
-    const title = 'La maison jungle'
-    return (
-        <div className='lmj-banner'>
-            <img src={monstera} alt='monstera-la-maison-jungle' className='lmj-monstera' />
-            <h1 className='lmj-title'>{title}</h1>
-        </div>
-    )
+function Banner({ children }) {
+    return <div className='lmj-banner'>{children}</div>
 }
+
 export default Banner
-
-
 
--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -1,51 +1,76 @@
+
+import monstera from '../assets/monstera.jpg'
 export const plantList = [
     {
         name: 'monstera',
         category: 'classique',
-        id: '1ed'
+        id: '1ed',
+        light: 2,
+        water: 3,
+        cover: monstera
     },
     {
         name: 'ficus lyrata',
         category: 'classique',
-        id: '2ab'
+        id: '2ab',
+        light: 3,
+        water: 1,
+        cover: monstera
     },
     {
         name: 'pothos argenté',
         category: 'classique',
-		id: '3sd',
-        isSpecialOffer: true
+        id: '3sd',
+        light: 1,
+        water: 2,
+        cover: monstera
     },
     {
         name: 'yucca',
         category: 'classique',
-        id: '4kk'
+        id: '4kk',
+        light: 3,
+        water: 1,
+        cover: monstera
     },
     {
         name: 'olivier',
         category: 'extérieur',
-		id: '5pl',
-        isAvailable: false
+        id: '5pl',
+        light: 3,
+        water: 1,
+        cover: monstera
     },
     {
         name: 'géranium',
         category: 'extérieur',
-        id: '6uo'
+        id: '6uo',
+        light: 2,
+        water: 2,
+        cover: monstera
     },
     {
         name: 'basilique',
         category: 'extérieur',
-		id: '7ie',
-        isSpecialOffer: true
+        id: '7ie',
+        light: 2,
+        water: 3,
+        cover: monstera
     },
     {
         name: 'aloe',
         category: 'plante grasse',
-		id: '8fp',
-        isSpecialOffer: true
+        id: '8fp',
+        light: 2,
+        water: 1,
+        cover: monstera
     },
     {
         name: 'succulente',
         category: 'plante grasse',
-        id: '9vn'
+        id: '9vn',
+        light: 2,
+        water: 1,
+        cover: monstera
     }
 ]

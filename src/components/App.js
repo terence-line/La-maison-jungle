1426934d9234a3780a1c639e738306a6a358d2c2
Teresa Soria <terence_line@hotmail.fr>
2022-10-03 00:42:00 +0200

    ajout fichiers

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,30 @@
+import { useState } from 'react'
 import Banner from './Banner'
 import logo from '../assets/logo.png'
-// import Cart from './Cart'
+import Cart from './Cart'
 import Footer from './Footer'
 import ShoppingList from './ShoppingList'
+import Time from './Time.js'
+import '../styles/Layout.css'
+
 function App() {
+  const [cart, updateCart] = useState([])
+  const [isFooterShown, updateIsFooterShown] = useState(true)
   return (
     <div>
       <Banner>
         <img src={logo} alt='La maison jungle' className='lmj-logo' />
         <h1 className='lmj-title'>La maison jungle</h1>
+        <Time />
       </Banner>
-
-      {/* <Cart /> */}
-      <ShoppingList />
-      <Footer />
+      <div className='lmj-layout-inner'>
+        <Cart cart={cart} updateCart={updateCart} />
+        <ShoppingList cart={cart} updateCart={updateCart} />
+        <button onClick={() => updateIsFooterShown(!isFooterShown)}>
+          Cacher !
+        </button>
+      </div>
+      {isFooterShown && <Footer cart={cart} />}
     </div>
   )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,51 @@
+
+
+import { useState, useEffect } from 'react'
 import '../styles/Footer.css'
 
-function Footer() {
-    return (
-        <footer className='lmj-footer'>
-            <div className='lmj-footer-elem'>
-                Pour les passionné·e·s de plantes 🌿🌱🌵
-            </div>
-            <div className='lmj-footer-elem'>Laissez-nous votre mail :</div>
-        </footer>
-    )
-}
 
-export default Footer
+    function Footer({ cart }) {
+        const [inputValue, setInputValue] = useState('')
+
+        useEffect(() => {
+            console.log(`1️⃣ Cette alerte s'affiche à chaque rendu`)
+        })
+
+        useEffect(() => {
+            console.log(`2️⃣ Cette alerte s'affiche au premier rendu`)
+        }, [])
+
+        useEffect(() => {
+            console.log(
+                `3️⃣ Cette alerte s'affiche la première fois et quand mon panier est mis à jour`
+            )
+        }, [cart])
+
+        useEffect(() => {
+            return () =>
+                console.log(`4️⃣ Cette alerte s'affiche quand Footer est retiré du DOM`)
+        }, [cart])
+        function handleInput(e) {
+            setInputValue(e.target.value)
+        }
+        function handleBlur() {
+            if (!inputValue.includes('@')) {
+                alert("Attention, il n'y a pas d'@, ceci n'est pas une adresse valide 😥")
+            }
+        }
+        return (
+            <footer className='lmj-footer'>
+                <div className='lmj-footer-elem'>
+                    Pour les passionné·e·s de plantes 🌿🌱🌵
+                </div>
+                <div className='lmj-footer-elem'>Laissez-nous votre mail :</div>
+                <input
+                    placeholder='Entrez votre mail'
+                    onChange={handleInput}
+                    value={inputValue}
+                    onBlur={handleBlur}
+                />
+            </footer>
+        )
+    }
+    export default Footer
